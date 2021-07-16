f3584a5bec28db7a5b3179f0f93185889c67be19
Hripsime Tepelikian <htepelikian@gmail.com>
2021-07-15 20:33:44 -0700

    generate markdown and package json added

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
+
+const questions = [
+    {
+        type: 'input',
+        message: "What is your GitHub username?",
+        name: 'username',
+        default: 'htepelikian',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid GitHub username is required.");
+            }
+            return true;
+        }
+    },
