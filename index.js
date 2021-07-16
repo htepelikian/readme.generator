e0569b5c166032774e737d78d1433a3a86cfed71
Hripsime Tepelikian <htepelikian@gmail.com>
2021-07-15 21:52:26 -0700

    index js update

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-const questions = [
-    {
-        type: 'input',
-        message: "What is your GitHub username?",
-        name: 'username',
-        default: 'htepelikian',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("A valid GitHub username is required.");
-            }
-            return true;
-        }
-    },
